15b546a3ebb5cb21b938a58727e0b313a6e07910
Wanialdo Eduardo de Lima da Silva <wanialdo@hotmail.com>
2020-02-10 10:36:36 -0300

    Commit Inicial

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.jogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.start2PlayersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jogoToolStripMenuItem,
+            this.ajudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(300, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // jogoToolStripMenuItem
+            // 
+            this.jogoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.start2PlayersToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.sairToolStripMenuItem});
+            this.jogoToolStripMenuItem.Name = "jogoToolStripMenuItem";
+            this.jogoToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.jogoToolStripMenuItem.Text = "Jogo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.novoToolStripMenuItem.Text = "Start 1 Player";
+            this.novoToolStripMenuItem.Click += new System.EventHandler(this.novoToolStripMenuItem_Click);
+            // 
+            // start2PlayersToolStripMenuItem
+            // 
+            this.start2PlayersToolStripMenuItem.Name = "start2PlayersToolStripMenuItem";
+            this.start2PlayersToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.start2PlayersToolStripMenuItem.Text = "Start 2 Players";
+            this.start2PlayersToolStripMenuItem.Click += new System.EventHandler(this.start2PlayersToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(153, 6);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            this.sobreToolStripMenuItem.Click += new System.EventHandler(this.sobreToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(100, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(200, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 124);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(100, 124);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(200, 124);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(0, 224);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox8.Location = new System.Drawing.Point(100, 224);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox9.Location = new System.Drawing.Point(200, 224);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(300, 324);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem jogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.ToolStripMenuItem start2PlayersToolStripMenuItem;
+    }
+}
+
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        #region campos globais
+        private int numPlayers = 1;
+        private int activePlayer = 0;
+        private int[,] game = new int[3, 3];
+        #endregion
+
+        #region métodos dos objetos
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statusPictureBox(false);
+        }
+
+        /// <summary>
+        /// Realiza uma jogada.
+        /// </summary>
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            //Essa linha não cria um objeto. Ela faz referência ao objeto.
+            PictureBox obj = (PictureBox)sender;
+
+            //Carrega a imagem do jogador.
+            if (activePlayer == 1)
+                obj.Image = System.Drawing.Image.FromFile("Circle.png");
+            else
+                obj.Image = System.Drawing.Image.FromFile("Xis.png");
+
+            //Faz com que a imagem tome todo o espaço do picturebox
+            obj.BackgroundImageLayout = ImageLayout.Zoom;
+
+            //Desliga o picturebox para não ser clicado novamente.
+            obj.Enabled = false;
+
+            //Substrig retorna parte do texto. Parametro padrão e o local de início.
+            //Length me retorna a quantidade de caracteres de um texto.
+            int pos = Convert.ToInt32(obj.Name.Substring(obj.Name.Length - 1));
+
+            game[Convert.ToInt32((pos - 1) / 3), ((pos - 1) % 3)] = activePlayer;
+
+            if (isVictory(activePlayer))
+            {
+                MessageBox.Show("Vitória do Jogador " + activePlayer.ToString());
+                statusPictureBox(false);
+            }
+            else
+            {
+                if (activePlayer == 1)
+                    activePlayer = 2;
+                else
+                    activePlayer = 1;
+
+                if ((numPlayers == 1) && (activePlayer == 2))
+                {
+                    ComputerPlay();
+                }
+            }
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startGame(1);
+        }
+
+        private void start2PlayersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            startGame(2);
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+
+        #region métodos gerais do jogo
+        /// <summary>
+        /// Verifica se há um vencedor no jogo.
+        /// </summary>
+        private bool isVictory(int player)
+        {
+            int lin = 0; int col = 0; int dg1; int dg2;
+            dg1 = 0;
+            dg2 = 0;
+
+            for (int i = 0; i <= 2; i++)
+            {
+                lin = 0;
+                col = 0;
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (game[i, j] == player) lin += 1;
+                    if (game[j, i] == player) col += 1;
+                    if ((game[i, j] == player) && (i + j == 2)) dg1 += 1;
+                    if ((game[i, j] == player) && (i == j)) dg2 += 1;
+                }
+                if ((lin == 3) || (col == 3) || (dg1 == 3) || (dg2 == 3)) return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Configura o início de uma partida
+        /// </summary>
+        private void startGame(int players)
+        {
+            numPlayers = players;
+            activePlayer = 1;
+            statusPictureBox(true);
+            game = new int[3, 3];
+        }
+
+        /// <summary>
+        /// Habilita ou desabilita as casas do jogo
+        /// </summary>
+        private void statusPictureBox(bool status)
+        {
+            foreach (Object control in this.Controls)
+            {
+                if (control.GetType() == new PictureBox().GetType())
+                {
+                    ((PictureBox)control).Enabled = status;
+                    ((PictureBox)control).Image = null;
+                }
+            }
+        }
+        #endregion
+
+        #region jogador computador
+        /// <summary>
+        /// Jogada realizada pelo computador.
+        /// </summary>
+        private void ComputerPlay()
+        {
+            //A pior parte, ensinar o computador a jogar.
+            bool played = false;
+            string[] jogadas = { "0,0;0,1;0,2", "1,0;1,1;1,2", "2,0;2,1;2,2", 
+                                  "0,0;1,0;2,0", "0,1;1,1;2,1", "0,2;1,2;2,2", 
+                                  "0,0;1,1;2,2", "0,2;1,1;2,0" };
+
+            //Anular a possibilidade de ganho do oponente.
+            for (int i = 0; i <= 7; i++)
+            {
+                if (WinRisk(jogadas[i], 1))
+                {
+                    ComputerMove(ChooseMove(jogadas[i]));
+                    played = true;
+                }
+            }
+
+            //Vencer se possível
+            if (!played)
+            {
+                for (int i = 0; i <= 7; i++)
+                {
+                    if (WinRisk(jogadas[i], 2))
+                    {
+                        ComputerMove(ChooseMove(jogadas[i]));
+                        played = true;
+                    }
+                }
+            }
+
+            //Nos outros casos a jogada é aleatória para evitar que o jogo sempre termine em empate.
+            if (!played)
+                ComputerMove(ChooseMove());
+        }
+
+        /// <summary>
+        /// Seleciona uma casa aleatória para realizar a jogada.
+        /// </summary>
+        private int ChooseMove()
+        {
+            //Gerar números aleatórios.
+            Random rnd = new Random();
+            int spot = 0;
+            try
+            {
+                string value = rnd.Next(1,10).ToString();
+                spot = Convert.ToInt32(value.Substring(0, 1));
+            }
+                //Coloquei o throw
+            catch (Exception ex)
+            {
+
+            }
+
+            if (spot == 0)
+                spot = 1;
+
+            if (game[Convert.ToInt32((spot - 1) / 3), ((spot - 1) % 3)] != 0)
+                spot = ChooseMove();
+
+            return spot;
+        }
+
+        /// <summary>
+        /// Seleciona uma casa em uma linha informada para jogar.
+        /// </summary>
+        private int ChooseMove(string boardLine)
+        {
+            string[] spots = { "0,0", "0,1", "0,2", "1,0", "1,1", "1,2", "2,0", "2,1", "2,2" };
+            int cont = 0;
+            foreach (string item in boardLine.Split(';'))
+            {
+                if (game[Convert.ToInt32(item.Split(',')[0]), Convert.ToInt32(item.Split(',')[1])] == 0) {
+                    for (int i = 0; i <= 8; i++)
+                    {
+                        if (spots[i] == item)
+                            cont = i + 1;
+                    }
+                }
+            }
+
+            return cont;
+        }
+
+        /// <summary>
+        /// Verifica se o jogador informado já preencheu duas casas na linha informada.
+        /// </summary>
+        private bool WinRisk(string boardLine, int player)
+        {
+            int cont = 0;
+            bool canplay = false;
+            foreach (string item in boardLine.Split(';'))
+            {
+                if (game[Convert.ToInt32(item.Split(',')[0]), Convert.ToInt32(item.Split(',')[1])] == player) cont++;
+                if (game[Convert.ToInt32(item.Split(',')[0]), Convert.ToInt32(item.Split(',')[1])] == 0) canplay = true;
+            }
+
+            if ((cont == 2) && (canplay))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Realiza a jogada do computador.
+        /// </summary>
+        private void ComputerMove(int numPictBox)
+        {
+            PictureBox obj = (PictureBox)this.Controls.Find("PictureBox" + numPictBox.ToString(), false)[0];
+            pictureBox_Click(obj, null);
+        }
+        #endregion
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
